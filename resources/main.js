30ad5b56ad81984f419fa52011a5b437162989f2
Leia <leiahopf@googlemail.com>
2020-05-16 18:45:29 +0100

    initial commit

---
+++ resources/main.js
@@ -0,0 +1,9 @@
+//user clicked on add button.
+//if there is any text inside the item field, add that text to the todo list.
+document.getElementById('add').addEventListener('click', function() {
+    var value = document.getElementById('item').value;
+ if(value){
+     console.log(value)
+ }
+})
+
